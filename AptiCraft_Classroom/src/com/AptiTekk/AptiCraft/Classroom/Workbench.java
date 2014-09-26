3256b9b1df5807059f70501e1bddef997bee0523
Mitch Talmadge <mitcht@aptitekk.com>
2014-09-26 01:01:17 -0600

    Added and sized header, menu, body, and footer. Added icon to window.

--- AptiCraft_Classroom/src/com/AptiTekk/AptiCraft/Classroom/Workbench.java
+++ AptiCraft_Classroom/src/com/AptiTekk/AptiCraft/Classroom/Workbench.java
@@ -2,6 +2,7 @@ package com.AptiTekk.AptiCraft.Classroom;
 
 import java.awt.AWTException;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
@@ -27,15 +28,41 @@ public class Workbench extends JFrame
 	JPanel contentPane = new JPanel(new BorderLayout());
 	this.setContentPane(contentPane);
 	
-	this.setMinimumSize(new Dimension(800, 600));
+	this.setMinimumSize(new Dimension(700, 450));
 	this.setMaximumSize(this.getMinimumSize());
 	this.setPreferredSize(this.getMaximumSize());
 	this.setResizable(false);
+	this.getContentPane().setBackground(new Color(230, 230, 230));
+	this.setIconImage(logoIcon);
+	
+	JPanel headerPanel = new JPanel();
+	headerPanel.setBackground(new Color(0,0,0,0));
+	headerPanel.setPreferredSize(new Dimension(700,50));
+	this.add(headerPanel, BorderLayout.NORTH);
+	
+	JPanel menuPanel = new JPanel();
+	menuPanel.setBackground(new Color(0,0,0,0));
+	menuPanel.setPreferredSize(new Dimension(60,334));
+	this.add(menuPanel, BorderLayout.WEST);
+	
+	JPanel bodyPanel = new JPanel();
+	bodyPanel.setBackground(new Color(0,0,0,0));
+	this.add(bodyPanel, BorderLayout.CENTER);
+	
+	JPanel footerPanel = new JPanel();
+	footerPanel.setBackground(new Color(0,0,0,0));
+	footerPanel.setPreferredSize(new Dimension(700,40));
+	this.add(footerPanel, BorderLayout.SOUTH);
 	
 	//Insert Content Here
 	
 	this.pack();
 	this.setVisible(false);
+	
+	System.out.println("Header: "+headerPanel.getSize());
+	System.out.println("Menu: "+menuPanel.getSize());
+	System.out.println("Body: "+bodyPanel.getSize());
+	System.out.println("Footer: "+footerPanel.getSize());
     }
     
     private void initSysTray()
