0282c06221ffcf2003697470a5a04e11c2f9311f
Mitch Talmadge <mitcht@aptitekk.com>
2015-04-07 23:31:19 -0600

    Reduced top and bottom margins of FontLabel

--- Classroom/src/main/java/com/AptiTekk/AptiCraft/Classroom/GUI/FontLabel.java
+++ Classroom/src/main/java/com/AptiTekk/AptiCraft/Classroom/GUI/FontLabel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
 
 public class FontLabel extends JLabel
 {
@@ -13,6 +14,7 @@ public class FontLabel extends JLabel
         super(text);
         this.setFont(font.deriveFont(fontSize));
         this.setForeground(fontColor);
+        this.setBorder(new EmptyBorder(-5,0,-5,0));
     }
     
 }
